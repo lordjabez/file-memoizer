72a01c9ce901bdf75b6d086ed309e3324961664f
Judson Neer <judson.neer@gmail.com>
2023-02-14 01:14:42 +0000

    Implement first functional version of the module.

--- file_memoizer/__init__.py
+++ file_memoizer/__init__.py
@@ -1,10 +1,9 @@
-"""TODO."""
+"""File Memoizer main module"""
 
 __version__ = '0.0.0'
 
 
 import datetime
-import functools
 import hashlib
 import json
 import os
@@ -13,24 +12,33 @@ import pathlib
 import cachier
 
 
-def _get_cache_key(*args):
-    """TODO."""
+_default_cache_directory = os.path.join(pathlib.Path.home(), '.file-memoizer', 'default')
+_default_cache_ttl = datetime.timedelta(days=1)
+
+
+def _get_cache_key_ignoring_unhashable(*args):
+    """
+    Get a cache key by producing a JSON string of the arguments then applying SHA256.
+
+    :param args: List of function arguments to hash
+    """
     args_hash = hashlib.sha256()
     key = json.dumps(list(args), default=lambda d: '', sort_keys=True)
     args_hash.update(key.encode())
     return args_hash.hexdigest()
 
 
-_cache_dir = os.path.join(pathlib.Path.home(), '.file-memoizer', __name__)
-_cache_ttl = datetime.timedelta(days=7)
-_cache_params = {
-    'cache_dir': _cache_dir,
-    'hash_params': _get_cache_key,
-    'stale_after': _cache_ttl,
-    'separate_files': True,
-}
+def memoize(cache_directory=None, cache_ttl=None, unhashable_args='fail'):
+    """
+    Get a memoization annotation function.
 
-
-def memoize():
-    """TODO."""
-    return functools.partial(cachier.cachier, **_cache_params)
+    :param cache_directory: Location to store cache files; defaults to a subfolder of home
+    :param cache_ttl: Time delta object of duration to keep cache files; defaults to a day
+    :param unhashable_args: Determine what to do with any unhashable args, either 'ignore' or 'fail' (the default)
+    """
+    cache_params = {'separate_files': True}
+    cache_params['cache_dir'] = cache_directory or _default_cache_directory
+    cache_params['stale_after'] = cache_ttl or _default_cache_ttl
+    if unhashable_args == 'ignore':
+        cache_params['hash_params'] = _get_cache_key_ignoring_unhashable
+    return cachier.cachier(**cache_params)
--- tests/test_file_memoizer.py
+++ tests/test_file_memoizer.py
@@ -1,6 +1,10 @@
-"""TODO."""
+"""File Memoizer test module"""
 
 
+import datetime
+import os
+import shutil
+import tempfile
 import toml
 
 import file_memoizer
@@ -10,7 +14,69 @@ with open('pyproject.toml', encoding='utf-8') as project_file:
     project = toml.load(project_file)
 
 
+_temp_directory = os.path.join(tempfile.gettempdir(), 'file-memoizer-test')
+_execution_flag_filename = os.path.join(_temp_directory, 'execution-flag')
+
+
+shutil.rmtree(file_memoizer._default_cache_directory, ignore_errors=True)  # pylint: disable=protected-access
+shutil.rmtree(_temp_directory, ignore_errors=True)
+
+
+def _set_execution_flag():
+    os.makedirs(_temp_directory, exist_ok=True)
+    with open(_execution_flag_filename, 'w', encoding='utf-8') as execution_file:
+        execution_file.write('executed')
+
+
+def _clear_execution_flag():
+    try:
+        os.remove(_execution_flag_filename)
+    except FileNotFoundError:
+        pass
+
+
+def _execution_flag_exists():
+    return os.path.isfile(_execution_flag_filename)
+
+
 def test_version():
     """Package version number is populated as expected."""
     version = project['tool']['poetry']['version']
     assert file_memoizer.__version__ == version
+
+
+def test_default_memoize():
+    """Default memoize annotation works as expected."""
+    _clear_execution_flag()
+
+    @file_memoizer.memoize()
+    def double(value):
+        _set_execution_flag()
+        return value * 2
+    result = double(1)
+    assert result == 2
+    assert _execution_flag_exists()
+    _clear_execution_flag()
+    result = double(1)
+    assert result == 2
+    assert not _execution_flag_exists()
+
+
+def test_custom_memoize():
+    """Customized memoize annotation works as expected."""
+    _clear_execution_flag()
+
+    cache_directory = _temp_directory
+    cache_ttl = datetime.timedelta(seconds=1)
+
+    @file_memoizer.memoize(cache_directory, cache_ttl, 'ignore')
+    def double(value):
+        _set_execution_flag()
+        return value * 2
+    result = double(1)
+    assert result == 2
+    assert _execution_flag_exists()
+    _clear_execution_flag()
+    result = double(1)
+    assert result == 2
+    assert not _execution_flag_exists()
