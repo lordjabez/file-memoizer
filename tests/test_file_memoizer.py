a84d01997c5f5383bf1ab4633d0b009f9411f2a9
Judson Neer <judson.neer@gmail.com>
2023-02-16 04:28:45 +0000

    Add more tests.

--- tests/test_file_memoizer.py
+++ tests/test_file_memoizer.py
@@ -5,7 +5,10 @@ import datetime
 import os
 import shutil
 import tempfile
+import time
 import toml
+
+import pytest
 
 import file_memoizer
 
@@ -14,16 +17,25 @@ with open('pyproject.toml', encoding='utf-8') as project_file:
     project = toml.load(project_file)
 
 
-_temp_directory = os.path.join(tempfile.gettempdir(), 'file-memoizer-test')
-_execution_flag_filename = os.path.join(_temp_directory, 'execution-flag')
+_execution_flag_directory = tempfile.gettempdir()
+_execution_flag_filename = os.path.join(_execution_flag_directory, 'execution-flag')
+
+_default_cache_directory = file_memoizer._default_cache_directory  # pylint: disable=protected-access
+_custom_cache_directory = os.path.join(tempfile.gettempdir(), 'file-memoizer-test')
+
+_custom_cache_ttl = datetime.timedelta(seconds=1)
 
 
-shutil.rmtree(file_memoizer._default_cache_directory, ignore_errors=True)  # pylint: disable=protected-access
-shutil.rmtree(_temp_directory, ignore_errors=True)
+def _clear_default_cache():
+    shutil.rmtree(_default_cache_directory, ignore_errors=True)
+
+
+def _clear_custom_cache():
+    shutil.rmtree(_custom_cache_directory, ignore_errors=True)
 
 
 def _set_execution_flag():
-    os.makedirs(_temp_directory, exist_ok=True)
+    os.makedirs(_execution_flag_directory, exist_ok=True)
     with open(_execution_flag_filename, 'w', encoding='utf-8') as execution_file:
         execution_file.write('executed')
 
@@ -39,6 +51,13 @@ def _execution_flag_exists():
     return os.path.isfile(_execution_flag_filename)
 
 
+def setup():
+    """Clear all storage before every test."""
+    _clear_default_cache()
+    _clear_custom_cache()
+    _clear_execution_flag()
+
+
 def test_version():
     """Package version number is populated as expected."""
     version = project['tool']['poetry']['version']
@@ -47,36 +66,82 @@ def test_version():
 
 def test_default_memoize():
     """Default memoize annotation works as expected."""
-    _clear_execution_flag()
 
     @file_memoizer.memoize()
     def double(value):
         _set_execution_flag()
         return value * 2
+
     result = double(1)
     assert result == 2
     assert _execution_flag_exists()
     _clear_execution_flag()
+
     result = double(1)
     assert result == 2
     assert not _execution_flag_exists()
-
-
-def test_custom_memoize():
-    """Customized memoize annotation works as expected."""
     _clear_execution_flag()
+    double.clear_cache()
 
-    cache_directory = _temp_directory
-    cache_ttl = datetime.timedelta(seconds=1)
+    result = double(1)
+    assert result == 2
+    assert _execution_flag_exists()
 
-    @file_memoizer.memoize(cache_directory, cache_ttl, 'ignore')
+
+def test_custom_directory():
+    """Customized cache file location is used when provided."""
+
+    @file_memoizer.memoize(cache_directory=_custom_cache_directory)
     def double(value):
         _set_execution_flag()
         return value * 2
+
+    assert double.cache_dpath() == _custom_cache_directory
+
+
+def test_custom_ttl():
+    """Customized cache time-to-live is used when provided."""
+
+    @file_memoizer.memoize(cache_ttl=_custom_cache_ttl)
+    def double(value):
+        _set_execution_flag()
+        return value * 2
+
     result = double(1)
     assert result == 2
     assert _execution_flag_exists()
     _clear_execution_flag()
+
     result = double(1)
     assert result == 2
     assert not _execution_flag_exists()
+    _clear_execution_flag()
+    time.sleep(2)
+
+    result = double(1)
+    assert result == 2
+    assert _execution_flag_exists()
+
+
+def test_unhashable_arg_fails():
+    """Unhashable argument causes an error by default."""
+
+    @file_memoizer.memoize()
+    def multiply(value_1, value_2):
+        _set_execution_flag()
+        return value_1 * value_2
+
+    with pytest.raises(TypeError):
+        multiply([1], 2)
+
+
+def test_unhashable_arg_ignored():
+    """Unhashable argument ignored when configured accordingly."""
+
+    @file_memoizer.memoize(unhashable_args='ignore')
+    def multiply(value_1, value_2):
+        _set_execution_flag()
+        return value_1 * value_2
+
+    result = multiply([1], 2)
+    assert result == [1, 1]
